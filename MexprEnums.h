8a2c092ddc66ce8422560f91fff452c7d98e0eef
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-04-06 20:53:40 +0900

    Added the struct for lex data and the function to dump the stack.

--- MexprEnums.h
+++ MexprEnums.h
@@ -2,14 +2,15 @@
 #define __MEXPR_ENUMS_
 
 typedef enum {
-    MATH_INT, /* an integer */
-    MATH_DOUBLE,
-    MATH_VARIABLE, /* alphanumeric one world */
-    MATH_DTYPE_END, /* the end indication of all enums */
+    INT, /* an integer */
+    DOUBLE,
+    VARIABLE, /* alphanumeric one world */
+    WHITE_SPACE,
+    DTYPE_END, /* the end indication of all enums */
 } mexppr_dtype;
 
 typedef enum {
-    PARSER_EOF = (int) MATH_DTYPE_END + 1,
+    PARSER_EOF = (int) DTYPE_END + 1,
 } extra_type;
 
 char *Mexpr_get_string_token(int token_code);
